2f4524a451881fdd790cad394ebf8b7eb00dc57f
Avishek Devnath <avishekdevnath@gmail.com>
2026-07-09 15:17:11 +0600

    feat: client wrappers for kickMember, setUserDisabled, targeted broadcast

--- src/features/admin/adminApi.js
+++ src/features/admin/adminApi.js
@@ -12,4 +12,7 @@ export const resetUser = (uid) => call('adminResetUser')({ uid })
 export const deleteUser = (uid) => call('adminDeleteUser')({ uid })
 export const closeRoom = (code) => call('adminCloseRoom')({ code })
 export const clearChat = (code) => call('adminClearChat')({ code })
-export const broadcast = (title, body, url) => call('adminBroadcast')({ title, body, ...(url ? { url } : {}) })
+export const broadcast = (title, body, url, toUid) =>
+  call('adminBroadcast')({ title, body, ...(url ? { url } : {}), ...(toUid ? { toUid } : {}) })
+export const kickMember = (code, slot) => call('adminKickMember')({ code, slot })
+export const setUserDisabled = (uid, disabled) => call('adminSetUserDisabled')({ uid, disabled })
